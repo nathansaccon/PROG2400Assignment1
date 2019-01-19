a534ec578455eba2d54fdf1f741fa8949946746a
Nathan Saccon <nathansaccon10@hotmail.com>
2019-01-18 22:07:29 -0500

    Empty Form Created
    App created with empty form

---
+++ PROG2400A1NathanSaccon/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PROG2400A1NathanSaccon.Models;
+
+namespace PROG2400A1NathanSaccon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PROG2400A1NathanSaccon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROG2400A1NathanSaccon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
